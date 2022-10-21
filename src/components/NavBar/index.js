2a7f209f794cef9e57e002a18e950f24b0a7da6a
Jitul Teron <jitulteron9@gmail.com>
2022-10-22 03:36:56 +0530

    main page frame done

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,16 +8,18 @@ function BrandExample() {
       <div className={classes.nav_container}>
         <div className={classes.cotainer}>          
             <div style={{ fontSize: '2rem', fontWeight: "650" }}>
-              <span style={{ color: '#1e24c4' }}>Super</span><span style={{ color: '#f34d76' }}>Mind</span>
+              <span style={{ color: '#1e24c4' }}>Medi</span><span style={{ color: '#f34d76' }}>G</span>
             </div>          
           <div className={classes.menu}>
             <Link to={"/"}>Home</Link>
-            <Link to={"/feature"}>Features</Link>
-            <Link to={"/price"}>Pricing</Link>
+            <Link to={"/feature"}>About us</Link>
+            <Link to={"/price"}>Feature</Link>
+            <Link to={"/price"}>Services</Link>
+            <Link to={"/feature"}>Mission</Link>            
           </div>
 
           <div className={classes.function}>
-            <Link to={"/"}>Get Started</Link> 
+            <Link to={"/"}>Login</Link> 
             <Link className={classes.getstart}>Get started</Link>
           </div>
         </div>
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,11 +1,41 @@
 import React from "react";
 import classes from "./services.module.css";
+import Box from "./Box";
+import img1 from "../../assets/7.png"
+import img2 from "../../assets/7.png"
+import img3 from "../../assets/7.png"
+import img4 from "../../assets/7.png"
+import img5 from "../../assets/7.png"
+import img6 from "../../assets/7.png"
 
-const Services = ()=>{
+const datas = [
+        {
+                img: img1,
+                header: "Greate Service",
+                text: "Download from our catalogue of 12487 acapellas, vocal samples & spoken word and start creating music today. We have male vocals, female acapellas, full songs or vocal hooks in a variety of tempos, genres and keys."
+        },
+        {
+                img: img2,
+                header: "Greate Service",
+                text: "Download from our catalogue of 12487 acapellas, vocal samples & spoken word and start creating music today. We have male vocals, female acapellas, full songs or vocal hooks in a variety of tempos, genres and keys."
+        },
+        {
+                img: img3,
+                header: "Greate Service",
+                text: "Download from our catalogue of 12487 acapellas, vocal samples & spoken word and start creating music today. We have male vocals, female acapellas, full songs or vocal hooks in a variety of tempos, genres and keys."
+        }
+]
+const Services = () => {
         return (
-        <div className={classes.container}>
-            
-        </div>)
+                <div className={classes.container}>
+                        {datas.map((info, key) => {
+                                return (<Box
+                                        img={info.img}
+                                        text={info.text}
+                                        header={info.header}
+                                />)
+                        })}
+                </div>)
 }
 
 export default Services;
