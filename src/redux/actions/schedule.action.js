a3d1187501c8c859c73c6f78c6f54540eaae5261
Jitul Teron <jitulteron9@gmail.com>
2022-10-30 11:07:33 +0530

    functionality and dashboard complete

--- src/redux/actions/schedule.action.js
+++ src/redux/actions/schedule.action.js
@@ -1,42 +1,44 @@
 import { scheduleConstant } from "../../redux/const";
+import { Toastify } from "../../App";
 
 export const submit_schedule = (schedule_input, schedules) => {
 
     return async (dispatch) => {
         try {
             console.log(schedule_input, schedules)
-            // dispatch({ type: scheduleConstant.SCHEDULE_REQUEST });
-            // // let res = await axios().post("/users", { ...user });
-            // let res;
-            // res = {
-            //     data: {
-            //         token: "test",
-            //         schedule_inputs: {
-            //             ...schedule_input,                        
-            //         },
-            //     },
-            // };
+            dispatch({ type: scheduleConstant.SCHEDULE_REQUEST });
+            // let res = await axios().post("/users", { ...user });
+            let res;
+            res = {
+                data: {
+                    token: "test",
+                    schedule_inputs: {
+                        ...schedule_input,
+                    },
+                },
+                status: 201
+            };
 
-            // if (res.status === 201) {
-            //     // Toastify("success", "Sign up successfully");
-            //     dispatch({ type: scheduleConstant.SCHEDULE_REQUEST });
-            //     const { token, schedule_inputs } = res.data;
+            if (res.status === 201) {
+                Toastify("success", "Sign up successfully");
+                dispatch({ type: scheduleConstant.SCHEDULE_REQUEST });
+                const { token, schedule_inputs } = res.data;
 
-            //     localStorage.setItem("token", token);
-            //     schedules.push(schedule_inputs)
-            //     localStorage.setItem("schedules", JSON.stringify(schedules));
-            //     dispatch({
-            //         type: scheduleConstant.SCHEDULE_SUCCESS,
-            //         payload: {
-            //             token,
-            //             schedules:schedules,
-            //         },
-            //     });
-            // } else {
-            //     const { error } = res.data;
-            //     // Toastify("error", "Unable to signup");
-            //     dispatch({ type: scheduleConstant.SCHEDULE_FAIL,payload: { error } });
-            // }
+                localStorage.setItem("token", token);
+                schedules.push(schedule_inputs)
+                localStorage.setItem("schedules", JSON.stringify(schedules));
+                dispatch({
+                    type: scheduleConstant.SCHEDULE_SUCCESS,
+                    payload: {
+                        token,
+                        schedules: schedules,
+                    },
+                });
+            } else {
+                const { error } = res.data;
+                // Toastify("error", "Unable to signup");
+                dispatch({ type: scheduleConstant.SCHEDULE_FAIL, payload: { error } });
+            }
         } catch (error) {
             // Toastify("error", error.response.data.message);
             dispatch({
